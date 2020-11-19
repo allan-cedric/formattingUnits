e42854520964a4184b4780e7a509b7ba8fe32abe
allan-cedric <allan.py3000@gmail.com>
2020-11-18 22:15:11 -0300

    Useless 'echo' removed

--- script.sh
+++ script.sh
@@ -5,7 +5,6 @@ echo '==================================================================='
 read -p 'Partition to format: ' partition
 declare -l choice
 read -p 'Are you sure (y/N)? ' choice
-declare -l validUnmount
 echo -e ''
 
 if [ "$choice" = "y" ]; then
@@ -17,7 +16,7 @@ if [ "$choice" = "y" ]; then
 		sudo mkfs -t vfat $partition 1>/dev/null
 		echo 'Formatting done!'
 	else
-		echo "Error: This partition does not exist."
+		echo 'Error: This partition does not exist.'
 	fi
 else
 	echo 'Nothing was done.'
